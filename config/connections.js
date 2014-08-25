9f3ddca062fd79a825291798a84f8acd7e926428
David Piry <davidpiry@gmail.com>
2014-08-25 21:31:26 +0200

    Basic mongo database configuration.

--- config/connections.js
+++ config/connections.js
@@ -65,6 +65,15 @@ module.exports.connections = {
     // database: 'your_mongo_db_name_here'
   },
 
+  mongodb: {
+    adapter: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    user: '',
+    password: '',
+    database: '242'
+  },
+
   /***************************************************************************
   *                                                                          *
   * PostgreSQL is another officially supported relational database.          *
